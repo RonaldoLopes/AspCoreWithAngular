96184f30c429f8ee304213b5ed8c8c862f9347a8
Ronaldo Cassiano Lopes Alves <ronaldorclamaster@gmail.com>
2019-06-27 14:35:28 -0300

    Web API P1 54

--- QuickBuy/QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
+++ QuickBuy/QuickBuy.Repositorio/Contexto/QuickBuyContexto.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using QuickBuy.Dominio.Entidades;
 using QuickBuy.Dominio.ObjetoDeValor;
+using QuickBuy.Repositorio.Config;
 
 namespace QuickBuy.Repositorio.Contexto
 {
@@ -16,6 +17,42 @@ namespace QuickBuy.Repositorio.Contexto
 
         public QuickBuyContexto(DbContextOptions options) : base(options)
         {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="modelBuilder"></param>
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Classes de mapeamento
+            modelBuilder.ApplyConfiguration(new UsuarioConfiguration());
+            modelBuilder.ApplyConfiguration(new ProdutoConfiguration());
+            modelBuilder.ApplyConfiguration(new PedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new ItemPedidoConfiguration());
+            modelBuilder.ApplyConfiguration(new FormaPagamentoConfiguration());
+
+            //carga inicial exemplo
+            modelBuilder.Entity<FormaPagamento>().HasData(
+            new FormaPagamento()
+            {
+                Id = 1, Nome = "Boleto", Descricao = "Forma de Pagamento Boleto",
+            },
+            new FormaPagamento()
+            {
+                Id = 2,
+                Nome = "Cartão de crédito",
+                Descricao = "Forma de Pagamento Cartão crédito",
+            },
+            new FormaPagamento()
+            {
+                Id = 3,
+                Nome = "Depósito",
+                Descricao = "Forma de Pagamento depósito",
+            }
+            );
+
+            base.OnModelCreating(modelBuilder);
         }
 
     }
